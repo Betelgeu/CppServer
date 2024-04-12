3e4dd1c1af7289106f5b32e1ac8075acaa90ae90
wuxiaojia <569268459@qq.com>
2024-04-12 19:06:30 +0800

    Add exception handling

--- server.cpp
+++ server.cpp
@@ -2,26 +2,48 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
+#include "util.h"
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+    errif(sockfd == -1, "socket create error");
+    // 1. create a socket
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
-
-    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
-    // SOMAXCONN = 128
-    listen(sockfd, SOMAXCONN);
-
+    // 2. bind the socket to an address
+    errif(bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket bind error");
+    // 3. listen for incoming connections
+    errif(listen(sockfd, SOMAXCONN) == -1, "socket listen error");
+    // 4. accept a connection
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_len = sizeof(clnt_addr);
     bzero(&clnt_addr, sizeof(clnt_addr));
 
     int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
+    errif(clnt_sockfd == -1, "socket accept error");
 
     printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+    while (true) {
+        char buf[1024];
+        bzero(&buf, sizeof(buf));
+    // 5. read from and write to the connection(echo server)
+        ssize_t read_bytes = read(clnt_sockfd, buf, sizeof(buf));
+        if(read_bytes > 0){
+            printf("message from client fd %d: %s\n", clnt_sockfd, buf);
+            write(clnt_sockfd, buf, sizeof(buf));
+        } else if(read_bytes == 0){
+            printf("client fd %d disconnected\n", clnt_sockfd);
+            close(clnt_sockfd);
+            break;
+        } else if(read_bytes == -1){
+            close(clnt_sockfd);
+            errif(true, "socket read error");
+        }
+    }
+    close(sockfd);
     return 0;
 }
